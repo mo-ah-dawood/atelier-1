e25682225f25e03f83155110ee81f44d17e1fca0
mo1inan <mohamed.kinan7@gmail.com>
2020-04-07 00:38:40 +0200

    text

--- lib/blocs/widgets.dart
+++ lib/blocs/widgets.dart
@@ -181,7 +181,7 @@ class _AtelierTextFieldState extends State<AtelierTextField> {
       crossAxisAlignment: CrossAxisAlignment.start,
       children: <Widget>[
         AnimatedDefaultTextStyle(
-          duration: mill0Second,
+          duration: Duration(microseconds: 0),
           style: TextStyle(
               color: widget.error != null ? Colors.red : hint,
               fontWeight: FontWeight.w600,
